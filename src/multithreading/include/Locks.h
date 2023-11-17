1a99304430cca787cb9c1b021b61e32b083ebecb
Carlos Osorio Merea <carlos.osoriomerea@ansys.com>
2023-11-16 19:31:27 -0600

    adding spinlock

--- src/multithreading/include/Locks.h
+++ src/multithreading/include/Locks.h
@@ -1,1 +1,19 @@
 #pragma once
+#include<atomic>
+
+class spinlock
+{
+private:
+    std::atomic_flag _flag;
+public:
+
+    void lock()
+    {
+        while(_flag.test_and_set()){}
+    }
+
+    void unlock()
+    {
+        _flag.clear();
+    }
+};
--- test/mutithreading/multithreadingTest.cpp
+++ test/mutithreading/multithreadingTest.cpp
@@ -1,6 +1,4 @@
-#include "Locks.h"
-
-
+#include<latch>
 
 int main()
 {
